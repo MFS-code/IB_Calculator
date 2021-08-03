f2851f89697f4311e73ba4bcc7ec8754d43face1
MFS-code <56933863+MFS-code@users.noreply.github.com>
2021-08-03 20:35:48 +0200

    Create how_does_it_work.py

---
+++ how_does_it_work.py
@@ -0,0 +1,27 @@
+from Grade_calculator.imports import *
+
+
+def Eng_HowDoesItWork():
+    popup("How does this work?",
+          put_html("<p>What this program does is to basically create an average between all of your grades in one "
+                   "criterion, sum it to the average of the other criterion, and then input that value into a table"
+                   " that looks something like this:&nbsp;</p>"
+                   """<a href="https://imgbb.com/"><img src="https://i.ibb.co/Q7B0rFd/ib.png" alt="ib" border="0"></a>"""
+                   "<p>The output of that table will be the grade you get out of ten, this is a simple process but it "
+                   "can be quite the drag to open the calculator, whatever database your school uses for grades"
+                   " a notepad, and the table at the same time and repeat"
+                   " the process with every subject, so I decided to create this tool to help make the process more "
+                   "bearable.</p>"))
+
+
+def Esp_HowDoesItWork():
+    popup("¿Cómo funciona?",
+          put_html("<p>El proceso que este programa hace es b&aacute;sicamente calcular la media entre todas las notas "
+                   "de un criterio, sumarlo a las medias de los otros criterios, y despu&eacute;s meter ese "
+                   "n&uacute;mero a una tabla como esta:</p>"
+                   """<p><a href="https://imgbb.com/"><img src="https://i.ibb.co/Q7B0rFd/ib.png" alt="ib" border="0" /></a></p>"""
+                   "<p>El resultado de esa tabla ser&aacute; tu nota sobre diez, el proceso es simple, "
+                   "pero puede hacerse pesado el tener que abrir la calculadora, la plataforma en la que tu colegio"
+                   " ponga las notas, la tabla, y un sitio donde apuntar cada vez que quieras calcular una nota,"
+                   " as&iacute; que he decidido crear esta herramienta para intentar hacer el proceso un poco m&aacute;s"
+                   " llevadero.</p>"))
